5ccf1154e160c7bd297b4c2f380bfde6279c269a
Anastasiya Ostertag <nastasja83@mail.ru>
2023-07-07 21:17:49 +0700

    add user greeting

---
+++ src/cli.js
@@ -0,0 +1,8 @@
+import readlineSync from 'readline-sync';
+
+const greeting = () => {
+    var userName = readlineSync.question('Welcome to the Brain Games!\nMay I have your name? ');
+    console.log('Hello, ' + userName + '!');
+}
+
+export default greeting;
