eafb030ff4b9c6381d0efd801f8fb82d23361ea8
Anastasiya Ostertag <nastasja83@mail.ru>
2023-07-10 17:50:56 +0700

    add brain-calc game

---
+++ src/games/brain-calc.js
@@ -0,0 +1,31 @@
+import runGame from '../index.js';
+
+const maxNumber = 20;
+const operations = ['-', '+', '*'];
+const description = 'What is the result of the expression?';
+
+const getRandomNumber = () => Math.floor(Math.random() * maxNumber);
+const getCorrectAnswer = (firstOperand, secondOperand, operation) => {
+  switch (operation) {
+    case '+':
+      return firstOperand + secondOperand;
+    case '-':
+      return firstOperand - secondOperand;
+    case '*':
+      return firstOperand * secondOperand;
+    default:
+      break;
+  }
+};
+
+const beginRound = () => {
+  const firstOperand = getRandomNumber();
+  const secondOperand = getRandomNumber();
+  const operation = operations[Math.floor(Math.random() * operations.length)];
+  const question = `Question: ${firstOperand} ${operation} ${secondOperand}`;
+  return [question, getCorrectAnswer(firstOperand, secondOperand, operation)];
+};
+
+const play = () => runGame(description, beginRound);
+
+export default play;
